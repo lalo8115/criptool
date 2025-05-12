b0fe04ef2f72b0c4e866ee917b42c02d49cbc24f
lalo8115 <89826691+lalo8115@users.noreply.github.com>
2025-05-11 22:49:59 -0600

    Update app.py

--- app.py
+++ app.py
@@ -49,4 +49,7 @@ def index():
     return render_template("index.html", resultado=resultado)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Usa el puerto que Render asigne, o 5000 por defecto
+    app.run(host="0.0.0.0", port=port, debug=True)
+
